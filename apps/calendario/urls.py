a2fb2cf3aa3385b4474307092dd2ebda6aa7dd75
pacifi <pacifi.bnr@gmail.com>
2015-11-26 16:23:03 -0500

    calendario

--- apps/calendario/urls.py
+++ apps/calendario/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', 'apps.calendario.views.calendar', name='panel'),
+    url(r'^json/$', CalendarJsonListView.as_view(), name='calendar_json'),
+  url(r'^calendario$', CalendarView.as_view(), name='calendar'),
     
 ]
