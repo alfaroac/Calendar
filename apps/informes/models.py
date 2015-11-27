cf84e2877ba7e298509a7c2260f3da46ef4552e7
alfaroac <alfaroac.is@gmail.com>
2015-11-27 03:09:14 -0500

    modificaciones

--- apps/informes/models.py
+++ apps/informes/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 class Institution(models.Model):
 	
-	codeInstitution=models.CharField(max_length=20)
+	codeInstitution=models.CharField(max_length=20, unique=True)
 	nameInstitution = models.CharField(max_length=100)
 	address = models.CharField(max_length=100)
 	latitude = models.CharField(max_length=20)
