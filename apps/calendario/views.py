55d480c317b33e74c4fc9d061ba4f7ed4655d6bd
alfaroac <alfaroac.is@gmail.com>
2015-11-27 08:07:53 -0500

    mi commit

--- apps/calendario/views.py
+++ apps/calendario/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Institution
-from .forms import InstitucionForm
+from .models import Activity
+from .forms import ActivityForm
 
 from django.core.urlresolvers import reverse
 
