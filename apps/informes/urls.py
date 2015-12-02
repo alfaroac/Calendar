2d045d7c88000ba594b0bd74a8577ae17dd0a569
alfaroac <alfaroac.is@gmail.com>
2015-12-02 16:54:10 -0500

    users maintenance

--- apps/informes/urls.py
+++ apps/informes/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     url(r'^institucion/registro/nuevo/$', 'apps.informes.views.addInstitution', name='nuevo'),    
     url(r'^institucion/editar/(?P<id>\d+)$', 'apps.informes.views.editInstitution', name='edit'),    
     url(r'^delete/(?P<id>\d+)$', 'apps.informes.views.deleteInstitution', name='delete'),
-    #url(r'^edit/(?P<pk>\d+)/$', ClienteUpdate.as_view(), name ='clientes_u'),
-    #    url(r'^delete/(?P<pk>\d+)/$', ClienteDelete.as_view(), name ='clientes_d'),
+    
 ]
--- apps/perfiles/urls.py
+++ apps/perfiles/urls.py
@@ -3,6 +3,8 @@ from .views import userRegister
 
 urlpatterns = [
     url(r'^$', 'apps.calendario.views.calendar', name='panel'),
-    url(r'^usuarios$', 'apps.perfiles.views.listaUsuarios', name='usuarios'),
-    url(r'^registrar$', userRegister.as_view(), name='registrar_usuario'),    
+    url(r'^usuarios$', 'apps.perfiles.views.listaUsuarios', name='users'),
+    url(r'^registrar$', userRegister.as_view(), name='registrar_usuario'),   
+    url(r'^usuarios/editar/(?P<id>\d+)$', 'apps.perfiles.views.editUsers', name='edit'),     
+    url(r'^usuarios/eliminar/(?P<id>\d+)$', 'apps.perfiles.views.deleteUsers', name='delete'),
 ]
